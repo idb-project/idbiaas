ed8e01b6cc7d6b644b9f4cc846720e465bccd56a
Ruben Schuller <schuller@bytemine.net>
2017-03-24 08:57:20 +0100

    add everything to requirements

--- setup.py
+++ setup.py
@@ -1,10 +1,29 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name="idbiaas",
     version="0.0.1",
     description="IDB IAAS adapter",
     packages=["idbiaas"],
-    install_requires=["requests","apache-libcloud"],
-    entry_points = { 'console_scripts': ['idbiaas=idbiaas.idbiaas:main'] }
+    install_requires=[
+        "requests",
+        "apache-libcloud",
+        "appdirs",
+        "argparse",
+        "asn1crypto",
+        "backports.ssl-match-hostname",
+        "cffi",
+        "cryptography",
+        "enum34",
+        "idna",
+        "ipaddress",
+        "libvirt-python",
+        "packaging",
+        "pyOpenSSL",
+        "pycparser",
+        "pyparsing",
+        "six",
+        "wsgiref"
+        ],
+    entry_points={'console_scripts':['idbiaas=idbiaas.idbiaas:main']}
 )
