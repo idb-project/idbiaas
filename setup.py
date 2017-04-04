7780b28409b94676c082e97f48e47232f798b3d3
Ruben Schuller <schuller@bytemine.net>
2017-04-04 09:14:23 +0200

    update version in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="idbiaas",
-    version="0.0.1",
+    version="0.0.2",
     description="IDB IAAS adapter",
     packages=["idbiaas"],
     author="bytemine GmbH",
